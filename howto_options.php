c5ac157520981b7d3ad4a1e20166b1beeffb19b0
christophe le drean <christophe@rouge-rose.net>
2012-12-10 16:15:13 +0100

    options

---
+++ howto_options.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin howto
+ * (c) 2012 christophe le drean
+ * Licence GNU/GPL
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+// en dev, pas de cache
+// define('_NO_CACHE', 1);
+
+// prefix cookie si spip est dans un sous-répertoire
+$cookie_prefix = "howto";
+
+
+
+?>
