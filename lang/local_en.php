69ea2327314ef6ce48d546274bc057d5da5a5e85
christophe le drean <christophe@rouge-rose.net>
2013-10-27 12:09:44 +0100

    ajouts : - index sur les rubriques - langues en et fr - styles sur la navigation prev/next

---
+++ lang/local_en.php
@@ -0,0 +1,13 @@
+<?php 
+if (!defined('_ECRIRE_INC_VERSION')) return;
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+    // C
+    'chapters' => 'Chapters',
+    
+    // N
+    'next' => 'Next',
+
+    // P
+    'previous' => 'Previous'
+    );
+?>
